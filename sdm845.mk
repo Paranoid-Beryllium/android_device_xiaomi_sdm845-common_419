933c54b26f609e277fc8310eff227b024ed91c4c
duckyduckG <duckyduckg65@gmail.com>
2025-08-09 20:23:14 +0500

    sdm845-common: Decommonize mixer_paths_tavil
    * OSS Audio hal does not support applying device specific audio configs and requires merging them.
    * So decommonize it for device specific changes.

--- sdm845.mk
+++ sdm845.mk
@@ -126,7 +126,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
     $(LOCAL_PATH)/audio/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
     $(LOCAL_PATH)/audio/usbv2_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usbv2_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
