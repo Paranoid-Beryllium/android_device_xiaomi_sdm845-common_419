3f7670f85cc8bf82eec150e3d6aead572eb89502
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-12-12 17:26:17 +0530

    sdm845-common: Drop LiveDisplay and UDFPS handlers
    * LiveDisplay is useless on an LCD Panel
    * Also we don't have UDFPS on beryllium so lets remove it
    
    Change-Id: I1a769d2d5b76441d0733139bdccc955949c8e97b
    Signed-off-by: Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -199,10 +199,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
