2bcbfefc4a57f57e8961ec7a4386246b52e885cc
imranpopz <imranpopz32@gmail.com>
2025-12-30 06:13:26 +0000

    sdm845-common: Build libjxl from source.
    * libjxl.so needed by camera lib libdng_sdk.so
    
    Change-Id: Id3c113fbdf94b6bbeec44f0579a06b4098d849d9

--- sdm845.mk
+++ sdm845.mk
@@ -129,6 +129,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
     libgui_shim \
+    libjxl \
+    libjxl.vendor \
     libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor
 
