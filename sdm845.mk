Kyuofox/device_xiaomi_sdm845-common

sdm845-common: Flash dummy super image if needed
* Needed because stock does not use dynamic partitions so
  there's no existing metadata that could be used

Change-Id: Icbc828389465f3d4681ca8e4c3adec782540752e

sdm845.mk
@@ -326,6 +326,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
     libqti-perfd-client

Paranoid-Beryllium/android_device_xiaomi_sdm845-common_419

Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

sdm845.mk
@@ -218,6 +218,8 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
 
 # Power
 PRODUCT_PACKAGES += \